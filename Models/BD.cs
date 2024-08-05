a2346b12393fd433e35a809f3d3b9738662b663e
iachuDyner <48313765@est-ort.edu.ar>
2024-08-05 10:34:43 -0300

    Conectamos la BD y empezamos la clase BD.cs

---
+++ Models/BD.cs
@@ -0,0 +1,24 @@
+using System.Data.SqlClient;
+using Dapper;
+public static class BD{
+
+public static void AgregarDeportista (Deportista dep){
+}
+public static void EliminarDeportista (int idDeportista){
+}
+public static void VerInfoDeporte (int idDeporte){
+}
+public static void VerInfoPais (int idPais){
+}
+public static void VerInfoDeportista (int idDeportista){
+}
+public static void ListarPaises (){
+}
+public static void ListarDeportistasDeporte (int idDeporte){
+}
+public static void ListarDeportistasPais (int idPais){
+}
+
+
+
+}
--- obj/Debug/net8.0/TP-JJOO-Dyner-Rozenbeim.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP-JJOO-Dyner-Rozenbeim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-JJOO-Dyner-Rozenbeim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a948ff3f106591c26390c16f1f688b6eeea8b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-JJOO-Dyner-Rozenbeim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-JJOO-Dyner-Rozenbeim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
