12e27226a9a4b9ef0b689653b3690d5e35fffea6
iachuDyner <48313765@est-ort.edu.ar>
2024-08-19 10:36:31 -0300

    mas avances de css

--- obj/Debug/net8.0/TP-JJOO-Dyner-Rozenbeim.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP-JJOO-Dyner-Rozenbeim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-JJOO-Dyner-Rozenbeim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb5dbb60bc9ff4beec112b254b5dcd4363f4cbd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e42079c8e4854ee091a2cfa82cb2c540b5d06f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-JJOO-Dyner-Rozenbeim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-JJOO-Dyner-Rozenbeim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
